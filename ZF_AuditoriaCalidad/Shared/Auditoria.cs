2f6b55572e52c3b27ff4e19f0260db1878019784
Leandro Gaido <lgaido34@gmail.com>
2021-06-16 15:53:42 -0300

    Entidades iniciales para la Auditoria

---
+++ ZF_AuditoriaCalidad/Shared/Auditoria.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ZF_AuditoriaCalidad.Shared
+{
+    [Table("tblAuditorias")]
+    public class Auditoria
+    {
+        public int ID { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; }
+
+        //Numero del Mes de la auditoria. Ej.: 8
+        public int Mes { get; set; }
+
+        //Numero del año de la auditoria. Ej.: 2020
+        public int Anio { get; set; }
+
+        [StringLength(5, ErrorMessage = "El formato de la hora tiene que ser HH:MM. Ej.: 09:45")]
+        public string Hora { get; set; }
+
+        [ForeignKey("Maquina")]
+        public int MaquinaID { get; set; }
+
+        //Nro de orden de produccion, dato obtenido al scannear el codigo de barras
+        public string NroOrden { get; set; }
+
+        //Nro de orden de produccion, dato obtenido al scannear el codigo de barras
+        public string NroPieza { get; set; }
+
+        //UserId del operario
+        [ForeignKey("Operario")]
+        public int OperarioID { get; set; }
+
+        //UserId del supervisor
+        [ForeignKey("Supervisor")]
+        public int SupervisorID { get; set; }
+
+        //UserId del auditor
+        public int UserID { get; set; }
+
+        public bool DeBaja { get; set; }
+
+        [DataType(DataType.Date)]
+        public DataType FechaBaja { get; set; }
+
+        public int UserBajaID { get; set; }
+
+        public virtual Maquina Maquina { get; set; }
+
+        public virtual Operario Operario { get; set; }
+
+        public virtual Operario Supervisor { get; set; }
+
+        public virtual List<DetalleAuditoria> DetallesAuditoria { get; set; }
+    }
+}
