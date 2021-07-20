9a8f8c983c008d3439669d7a1b68e2e5b1a1fcf2
javisicardi <javisicardi94@gmail.com>
2021-07-20 09:56:10 -0300

    agregado roles y usuarios de prueba

---
+++ ZF_AuditoriaCalidad/Server/Data/ApplicationDbInitialiser.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZF_AuditoriaCalidad.Server.Models;
+
+namespace ZF_AuditoriaCalidad.Server.Data
+{
+    public static class ApplicationDbInitialiser
+    {
+        public static void SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            AddRoleIfNotExists(roleManager, "Administrador");
+            AddRoleIfNotExists(roleManager, "Auditor");
+        }
+
+        public static void SeedUsers(UserManager<ApplicationUser> userManager)
+        {
+            (string name, string password, string role)[] demoUsers = new[]
+            {
+                (name: "admin@gmail.com", password: "Adminzf.20", role: "Administrador"),
+                (name: "auditor1@gmail.com", password: "Auditor1.18", role: "Auditor"),
+                (name: "auditor2@gmail.com", password: "Auditor2.18", role: "Auditor"),
+            };
+
+            foreach ((string name, string password, string role) user in demoUsers)
+            {
+                AddUserIfNotExists(userManager, user);
+            }
+
+        }
+
+        private static void AddUserIfNotExists(UserManager<ApplicationUser> userManager, (string name, string password, string role) demoUser)
+        {
+            ApplicationUser user = userManager.FindByNameAsync(demoUser.name).Result;
+            if (user == default)
+            {
+                var newAppUser = new ApplicationUser
+                {
+                    UserName = demoUser.name,
+                    Email = demoUser.name,
+                    EmailConfirmed = true
+                };
+                _ = userManager.CreateAsync(newAppUser, demoUser.password).Result;
+                if (!string.IsNullOrWhiteSpace(demoUser.role))
+                {
+                    var roles = demoUser.role.Split(',').Select(a => a.Trim()).ToArray();
+                    Console.WriteLine($"{roles.Length}");
+                    foreach (var role in roles)
+                    {
+                        _ = userManager.AddToRoleAsync(newAppUser, role).Result;
+
+                    }
+                }
+
+            }
+        }
+
+        private static void AddRoleIfNotExists(RoleManager<IdentityRole> roleManager, string roleName)
+        {
+            if (roleManager.FindByNameAsync(roleName).Result == default)
+            {
+                roleManager.CreateAsync(new IdentityRole { Name = roleName }).Wait();
+            }
+        }
+    }
+}
