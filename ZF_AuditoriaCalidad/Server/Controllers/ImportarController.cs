e2425918200558500a97ad5fdf396ad6379d8593
Leandro Gaido <lgaido34@gmail.com>
2021-08-24 12:08:51 -0300

    Bak para subir archivo al excel

--- ZF_AuditoriaCalidad/Server/Controllers/ImportarController.cs
+++ ZF_AuditoriaCalidad/Server/Controllers/ImportarController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,5 +13,39 @@ namespace ZF_AuditoriaCalidad.Server.Controllers
     [ApiController]
     public class ImportarController : ControllerBase
     {
+        private readonly IHostEnvironment _environment;
+
+        public ImportarController(IHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public async Task<IActionResult> Post([FromForm]IFormFile File)
+        {
+            if(File == null || File.Length == 0)
+            {
+                return BadRequest("File Empty or Null");
+            }
+
+            string newFileName = File.FileName;
+            string extension = Path.GetExtension(newFileName);
+
+            string[] allowedExtensions = new string[] { ".xlsx" };
+
+            if (!allowedExtensions.Contains(extension))
+            {
+                return BadRequest("File not valid");
+            }
+
+            string fileName = $"{Guid.NewGuid()}{extension}";
+            string filePath = Path.Combine(_environment.ContentRootPath, "UploadedFile", newFileName);
+
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            {
+                await File.CopyToAsync(fileStream);
+            }
+
+            return Ok($"UploadedFile/{newFileName}");
+        }
     }
 }
