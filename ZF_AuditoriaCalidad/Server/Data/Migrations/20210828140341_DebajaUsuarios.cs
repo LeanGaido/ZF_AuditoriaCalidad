27aaa96f36ce0f559561e6d14968c5c2d03075d7
Leandro Gaido <lgaido34@gmail.com>
2021-08-30 09:12:18 -0300

    migracion para dar de baja a los usuarios

---
+++ ZF_AuditoriaCalidad/Server/Data/Migrations/20210828140341_DebajaUsuarios.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ZF_AuditoriaCalidad.Server.Data.Migrations
+{
+    public partial class DebajaUsuarios : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "DeBaja",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaBaja",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserBajaId",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeBaja",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FechaBaja",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserBajaId",
+                table: "AspNetUsers");
+        }
+    }
+}
