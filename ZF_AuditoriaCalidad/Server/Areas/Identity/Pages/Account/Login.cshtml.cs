cee4f7013efc655a4d2737fbdca4edde3bf8b9c5
Leandro <lgaido34@gmail.com>
2021-08-31 14:27:33 -0300

    validacion en el login para usuarios Auditores

--- ZF_AuditoriaCalidad/Server/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ ZF_AuditoriaCalidad/Server/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -80,9 +80,21 @@ namespace ZF_AuditoriaCalidad.Server.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+
+                var usuarioAux = await _userManager.FindByNameAsync(Input.Email);
+                if (await _userManager.IsInRoleAsync(usuarioAux, "Auditor"))
+                {
+                    var claimsUsuarioAux = await _userManager.GetClaimsAsync(usuarioAux);
+                    if (claimsUsuarioAux.Where(x => x.Type == "Auditor").FirstOrDefault() == null)
+                    {
+                        ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                        return Page();
+                    }
+                }
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
+
                     _logger.LogInformation("User logged in.");
                     return LocalRedirect(returnUrl);
                 }
